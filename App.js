fd292b6ecde987b0a5e7cb747ebc1ba7c365b424
Daniel Kirk <imdanielkirk@gmail.com>
2023-06-05 20:41:07 -0700

    creating mobile app

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+//View -> UIView
 export default function App() {
+
+  console.log("App executed!");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>I love you baby!</Text>
       <StatusBar style="auto" />
     </View>
   );
